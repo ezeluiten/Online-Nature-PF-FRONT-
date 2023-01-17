06a6aa507915ab18b7af14c05289676522150c61
Ghoul-JS <jmanuelgut170@gmail.com>
2023-01-17 11:58:30 -0500

    creat react app

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,9 @@
+import style from './landingPage.module.css'
+
+export default function LandingPage() {
+    return(
+        <div className={style.container}>
+            <h1>SOY EL LANDING PAGE!</h1>
+        </div>
+    )
+}
