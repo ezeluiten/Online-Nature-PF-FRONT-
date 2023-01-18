c1b464d277f0affa8b683d9ea2b8310b46f87ee8
Ezequiel Luiten <ezequiel.luiten@gmail.com>
2023-01-18 18:07:47 -0300

    up to date

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import style from './landingPage.module.css'
 
 export default function LandingPage() {
     return(
-        <div className={style.container} >
+        <div nnclassName={style.container} >
             <h1>SOY EL LANDING PAGE!</h1>
         </div>
     )
