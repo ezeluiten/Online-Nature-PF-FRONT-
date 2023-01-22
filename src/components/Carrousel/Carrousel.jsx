f9158ff0411b9d67f2291b7ebac9504dd1e83438
danicorderoo <soydcordero@gmail.com>
2023-01-22 16:48:27 -0400

    configuracion de useNavegate de welcome

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,23 +1,29 @@
 import Carousel from "react-bootstrap/Carousel";
 import style from "./Carrusel.module.css";
+import { useNavigate } from "react-router-dom";
 const img1 = require("../../imagenes/amazonas.jpg");
 const img2 = require("../../imagenes/koala.webp");
 const img3 = require("../../imagenes/mono.jpg");
 const logo = require("../../imagenes/logo2.png");
 function CarouselFadeExample() {
+  const navigate = useNavigate();
   return (
     <Carousel fade>
       <Carousel.Item>
         <img className={style.imagen} src={img1} alt="First slide" />
         <Carousel.Caption>
-          <img className={style.logo} src={logo} alt="img_logo" />
-          <h3>First slide label</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
-            quia culpa fugit saepe atque, exercitationem nesciunt. Sapiente
-            voluptates temporibus a.
-          </p>
-          <button className={style.boton}>WELCOME</button>
+          <div className={style.container}>
+            <img className={style.logo} src={logo} alt="img_logo" />
+            <h3>First slide label</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quia culpa fugit saepe atque, exercitationem nesciunt. Sapiente
+              voluptates temporibus a.
+            </p>
+            <button onClick={() => navigate("/login")} className={style.boton}>
+              WELCOME
+            </button>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -31,7 +37,9 @@ function CarouselFadeExample() {
             quia culpa fugit saepe atque, exercitationem nesciunt. Sapiente
             voluptates temporibus a.
           </p>
-          <button className={style.boton}>WELCOME</button>
+          <button onClick={() => navigate("/login")} className={style.boton}>
+            WELCOME
+          </button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -45,7 +53,9 @@ function CarouselFadeExample() {
             quia culpa fugit saepe atque, exercitationem nesciunt. Sapiente
             voluptates temporibus a.
           </p>
-          <button className={style.boton}>WELCOME</button>
+          <button onClick={() => navigate("/login")} className={style.boton}>
+            WELCOME
+          </button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
