48e65b735eda79c18930c4e6476a01223e1a9c64
Ghoul-JS <jmanuelgut170@gmail.com>
2023-01-22 17:54:28 -0500

    creando los details (en proceso)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.jsx';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
-import Detail  from './components/detail/detail.jsx';
+import Detail  from './components/detail/AnimalDetail/AnimalDetail.jsx';
 import { Home } from './components/Home/Home.jsx';
 
 function App() {
   return (
     // <Route path="/" element={<Dashboard />}>
     <BrowserRouter>
-    <Routes>
-    <Route exact path = '/detail/:id' element={<Detail/>}/>
-      <Route exact path = '/home' element={<Home/>}/>
-      <Route exact path = '/' element = {<LandingPage/>}/>
+      <Routes>
+        <Route path = '/detail/:id' element={<Detail/>}/>
+        <Route path = '/home' element={<Home/>}/>
+        <Route default path = '/' element = {<LandingPage/>}/>
       </Routes>
     </BrowserRouter>
-      
-    
   );
 }
 
