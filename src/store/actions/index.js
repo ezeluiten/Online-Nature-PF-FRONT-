77e3b59a9e708ba607838db19a4b5250fe913cb1
Ezequiel Luiten <ezequiel.luiten@gmail.com>
2023-01-22 23:49:34 -0300

    actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const  getanimals = () => {
+export const  getAnimals = () => {
   return async function (dispatch) {
     try {
       const response = await axios.get("http://localhost:3001/api/v1/animals");
