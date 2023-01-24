3d7c4b9e54936061cdbf4e5c00b1d69ce9a1a1bf
Ghoul-JS <jmanuelgut170@gmail.com>
2023-01-23 19:16:55 -0500

    algunos ajustes a estilos y componente nav home y campaign con media querys de mobile (falta arreglar el dropdown de nav en mobile size)

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ import { Home } from './components/Home/Home.jsx';
 import { Navigation } from "./components/routes/Navigation"
 import axios from "axios"
 
-// axios.defaults.baseURL = "http://localhost:3001/api/v1" 
-axios.defaults.baseURL = "https://craven-sign-production.up.railway.app/api/v1/"
+axios.defaults.baseURL = "http://localhost:3001/api/v1" 
+// axios.defaults.baseURL = "https://craven-sign-production.up.railway.app/api/v1/"
 
 function App() {
   return (
-    // <Route path="/" element={<Dashboard />}>
     <Navigation/>
-    // <BrowserRouter>
-    // <Routes>
-    // <Route exact path = '/detail/:id' element={<Detail/>}/>
-    //   <Route exact path = '/home' element={<Home/>}/>
-    //   <Route exact path = '/' element = {<LandingPage/>}/>
-    //   </Routes>
-    // </BrowserRouter>
   );
 }
 
