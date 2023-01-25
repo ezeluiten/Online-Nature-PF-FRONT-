a85224bf2e67f3b32371cdc95af9c62789cdaca9
Esteban Puentes <esteban.puentes@bewe.io>
2023-01-25 06:16:05 -0500

    se creo la logica para el componente de carrito de compras

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Detail  from './components/detail/AnimalDetail/AnimalDetail.jsx';
 import { Home } from './components/Home/Home.jsx';
 import { Navigation } from "./components/routes/Navigation"
 import axios from "axios"
-import { ModalCashierPortal } from "./components/Cashier/ModalCashier"
+import { ModalCashierPortal } from "./components/Cashier/ModalCashier/ModalCashier"
 import { HandleClose } from '../src/components/helpers/cashierModalHelper.js';
 import { useDispatch, useSelector } from "react-redux"
 import { setOpenModal } from "./store/actions"
-
+import { CardShoppingCart } from './components/Cashier/ShoppingCart/CardShoppingCart.js';
 
 
 const developUrl = process.env.REACT_APP_DEVELOPMENT_URL
@@ -29,9 +29,8 @@ function App() {
   }
   return (
     <>
-      <ModalCashierPortal onClose={modifyModalState}>
-        {/* {componente - children} */}
-      </ModalCashierPortal>
+      <ModalCashierPortal onClose={modifyModalState} children={<CardShoppingCart/>}/>
+        
       <Navigation/>
     </>
   );
