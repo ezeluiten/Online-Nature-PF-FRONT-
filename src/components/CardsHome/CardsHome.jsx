0c95dc78650d960bfc37d1d8eddae9a2e0bf83f7
Esteban Puentes <esteban.puentes@bewe.io>
2023-02-04 21:43:06 -0500

    modificando estilos

--- src/components/CardsHome/CardsHome.jsx
+++ src/components/CardsHome/CardsHome.jsx
@@ -7,40 +7,42 @@ const img2 = require("../../imagenes/adopta.jpg");
 const img3 = require("../../imagenes/donate.png");
 
 export default function CardsHome(props) {
-  return (
-    <>
-      <div className={style.contenedor}>
-        <div className={style.card}>
-          <Card style={{ width: "18rem", height: "24rem"}}>
-            <Card.Img
-              variant="top"
-              src={img1}
-              style={{ width: "18rem", height: "150px" }}
-              className={"cardImg"}
-            />
-            <Card.Body>
-              <Card.Text>
+	return (
+		<>
+			<div className={style.contenedor}>
+				<div className={style.card}>
+					<Card style={{ width: "20rem", height: "28rem" }}>
+						<Card.Img
+							variant="top"
+							src={img1}
+							style={{ width: "20rem", height: "150px" }}
+							className={"cardImg"}
+						/>
+						<div className={style.cardContainer}>
+							<Card.Text className="card-text-container">
 								<Card.Title className={style.tittle}>Plant a tree!</Card.Title>
 								<h3 className={style.sabias}>Did you know?...</h3>
 								Planting a tree can help reduce air pollution. air and improve
 								air quality.
 							</Card.Text>
-              <Button className={style.store} variant="primary">
+							<br />
+							<br />
+							<button className={style.store} variant="primary">
 								Know more
-							</Button>
-            </Card.Body>
-          </Card>
-        </div>
+							</button>
+						</div>
+					</Card>
+				</div>
 
-        <div className={style.card}>
-        <Card style={{ width: "18rem", height: "24rem" }}>
-          <Card.Img
-            variant="top"
-            src={img2}
-            style={{ width: "18rem", height: "150px" }}
-          />
-          <Card.Body>
-              <Card.Text>
+				<div className={style.card}>
+					<Card style={{ width: "20rem", height: "28rem"}}>
+						<Card.Img
+							variant="top"
+							src={img2}
+							style={{ width: "20rem", height: "150px" }}
+						/>
+						<div className={style.cardContainer}>
+							<Card.Text>
 								<Card.Title className={style.tittle}>
 									Adopt an animal!
 								</Card.Title>
@@ -48,22 +50,22 @@ export default function CardsHome(props) {
 								Adopting an endangered animal can virtually help support
 								conservation and protection efforts for that species.
 							</Card.Text>
-            <Button className={style.store} variant="primary">
-              Store
-            </Button>
-          </Card.Body>
-        </Card>
-        </div>
+							<button className={style.store} variant="primary">
+								Know more
+							</button>
+						</div>
+					</Card>
+				</div>
 
-        <div className={style.card}>
-          <Card style={{ width: "18rem", height: "24rem" }}>
-            <Card.Img
-              variant="top"
-              src={img3}
-              style={{ width: "18rem", height: "150px" }}
-            />
-            <Card.Body>
-            <Card.Text>
+				<div className={style.card}>
+					<Card style={{ width: "20rem", height: "28rem" }}>
+						<Card.Img
+							variant="top"
+							src={img3}
+							style={{ width: "19.5rem", height: "150px" }}
+						/>
+						<div className={style.cardContainer}>
+							<Card.Text>
 								<Card.Title className={style.tittle}>
 									Donate is Save!
 								</Card.Title>
@@ -71,14 +73,13 @@ export default function CardsHome(props) {
 								Donating to a species conservation organization can help fund
 								endangered animal research and protection projects
 							</Card.Text>
-              <Button className={style.store} variant="primary">
-                Store
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
-
-      </div>
-    </>
-  );
+							<button className={style.store} variant="primary">
+								Know more
+							</button>
+						</div>
+					</Card>
+				</div>
+			</div>
+		</>
+	);
 }
