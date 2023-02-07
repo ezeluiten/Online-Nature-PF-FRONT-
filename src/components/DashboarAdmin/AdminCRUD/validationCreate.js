8c10c322c4cab437b57d52d38414cdfb269c3c94
Ghoul-JS <jmanuelgut170@gmail.com>
2023-02-07 13:38:34 -0500

    añadi validaciones al form create

--- src/components/DashboarAdmin/AdminCRUD/validationCreate.js
+++ src/components/DashboarAdmin/AdminCRUD/validationCreate.js
@@ -2,7 +2,9 @@ export default function validationCreate(creatreFormItem) {
     const errors = {};
     const regexTitle = /^[a-zA-Z\s]+$/;
     const regexImageUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?.(png|jpg|jpeg)$/
-
+    
+    if(!creatreFormItem.item_type[0]) errors.item_type = 'You must choose at least one item type'
+    
     if(!regexTitle.test(creatreFormItem.title)) errors.title = 'Only letters are allowed';
     else if(creatreFormItem.title.length < 5) errors.title = 'You must type more than 5 characters'
     
@@ -17,7 +19,6 @@ export default function validationCreate(creatreFormItem) {
     
     if(creatreFormItem.amount < 1000) errors.amount = 'amount must be greater than 1000'
 
-    if(!creatreFormItem.item_type[0]) errors.item_type = 'You must choose at least one item type'
 
     return errors
 }
